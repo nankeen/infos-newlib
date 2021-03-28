1bf92d8c754629900501bf57c2c88fe62c965ee3
Kai <nankeen@users.noreply.github.com>
2021-03-28 16:50:21 +0100

    Add command line arguments to crt0.c

--- newlib/libc/sys/infos/crt0.c
+++ newlib/libc/sys/infos/crt0.c
@@ -1,14 +1,15 @@
 #include "syscall.h"
  
 extern void exit(int code);
-extern int main();
+extern int main(int argc, const char *argv[]);
  
-void _start()
+void _start(const char *cmdline)
 {
     __console_handle = open("/dev/console", 0, 0);
     if (is_error(__console_handle)) exit(1);
     
-    int _exit_code = main();
+    const char* argv[] = {cmdline, 0};
+    int _exit_code = main(0, argv);
     close(__console_handle);
     exit(_exit_code);
 }
